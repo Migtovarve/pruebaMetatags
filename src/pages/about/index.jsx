d855df569ece97cf8c866040cb633e3a9c7eaf2a
Miguel Tovar <migtovarve@gmail.com>
2024-06-26 14:56:14 -0400

    meta tags

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import React from 'react'
 
+const origin = (typeof window === 'undefined') ?  '' : window.location.origin
+
 const index = () => {
+
+    console.log({origin})
   return (
     <>
     <Head>
         <meta property="og:title" content="How to Become an SEO Expert (8 Steps)" />
         <meta property="og:description" content="Get from SEO newbie to SEO pro in 8 simple steps." />
-        <meta property="og:image" content="https://ahrefs.com/blog/wp-content/uploads/2019/12/fb-how-to-become-an-seo-expert.png" />
+        <meta property="og:image" content={`${origin}/img/android-chrome-384x384.png`} />
     </Head>
     <div>index</div>
     </>
