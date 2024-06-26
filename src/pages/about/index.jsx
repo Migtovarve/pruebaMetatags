967167d5e3e614742ef9577d31f39d8beb9bda05
Miguel Tovar <migtovarve@gmail.com>
2024-06-26 15:03:26 -0400

    test1

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -2,6 +2,12 @@ import Head from 'next/head'
 import React from 'react'
 
 const origin = (typeof window === 'undefined') ?  '' : window.location.origin
+const description = null
+const des2 = 'JASDASM ASDIJSA DASdas mdasd kjasdjasoidjasi'
+
+const description2 = {
+    name: des2
+}
 
 const index = () => {
 
@@ -10,8 +16,8 @@ const index = () => {
     <>
     <Head>
         <meta property="og:title" content="How to Become an SEO Expert (8 Steps)" />
-        <meta property="og:description" content="Get from SEO newbie to SEO pro in 8 simple steps." />
-        <meta property="og:image" content={`${origin}/img/android-chrome-384x384.png`} />
+        <meta property="og:description" content={description || description2?.name} />
+        <meta property="og:image" content={`${origin}/android-chrome-384x384.png`} />
     </Head>
     <div>index</div>
     </>
